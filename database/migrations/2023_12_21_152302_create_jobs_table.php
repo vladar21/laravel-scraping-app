1d78746f24b2129e9f5231c7c03db856d16d1144
vladar21 <vladar21@gmail.com>
2023-12-21 17:24:06 +0200

    create jobs migrations

---
+++ database/migrations/2023_12_21_152302_create_jobs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->json('urls'); // Storing array of URLs
+            $table->json('selectors'); // HTML/CSS selectors
+            $table->json('scraped_data')->nullable(); // Store the scraped data
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
