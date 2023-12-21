b765975c9e43e0ad378a37da780a6f8ba4c572a7
vladar21 <vladar21@gmail.com>
2023-12-21 17:32:52 +0200

    create jobs controller

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Http\Request;
+
+class JobController extends Controller
+{
+    public function store(Request $request)
+    {
+        // Validate and create a job
+    }
+
+    public function show($id)
+    {
+        // Retrieve and return a job by ID
+    }
+
+    public function destroy($id)
+    {
+        // Delete a job by ID
+    }
+}
+
